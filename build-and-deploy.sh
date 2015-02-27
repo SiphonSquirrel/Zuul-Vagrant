368ec2c5d98f1a4144912c338e91c1f538f39683
SiphonSquirrel <SiphonSquirrel@users.noreply.github.com>
2015-02-26 21:23:06 -0500

    Added check for settings.xml in /vagrant.

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+if [[ ! -f $HOME/.m2/settings.xml ]]; then
+	if [[ -f /vagrant/settings.xml ]]; then
+		mkdir -p $HOME/.m2
+		cp /vagrant/settings.xml $HOME/.m2
+	fi
+fi
+
 cd /vagrant/Zuul-Src || exit 1
 mvn install || exit 1
 sudo service tomcat7 stop || exit 1
